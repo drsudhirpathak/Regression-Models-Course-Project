ea517f35775caa7f441693566d5cf50d8abb5b1f
drsudhirpathak <32571627+drsudhirpathak@users.noreply.github.com>
2017-10-12 20:40:37 +0530

    Update cars.R

--- cars.R
+++ cars.R
@@ -7,8 +7,6 @@ mtcars$am <- factor(mtcars$am)
 mtcars$gear <- factor(mtcars$gear)
 mtcars$carb <- factor(mtcars$carb)
 str(mtcars)
-
-
 ## ------------------------------------------------------------------------
 library(plyr)
 library(ggplot2)
@@ -18,19 +16,14 @@ ggplot(mtcars, aes(x=transmission, y=mpg, fill=transmission)) +
     geom_boxplot() +
     xlab("Transmission type") +
     ylab("Miles per gallon")
-
-
 ## ------------------------------------------------------------------------
 fit1 <- lm(mpg ~ am, data=mtcars)
 summary(fit1)
-
-
 ## ----, warning=FALSE, fig.width=8, fig.height=6--------------------------
 pairs(mtcars, panel=function(x,y) {
     points(x, y)
     abline(lm(y ~ x), col="red")
 })
-
 
 ## ------------------------------------------------------------------------
 initial_model <- lm(mpg ~ ., data=mtcars)
@@ -38,4 +31,3 @@ best_model <- step(initial_model, direction="both", trace=0)
 summary(best_model)
 par(mfrow = c(2,2))
 plot(best_model)
-
